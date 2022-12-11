1b1b97874e3eadb83b53a8c661615b738cd454c5
Anastasya <shamr44@mail.ru>
2022-12-11 16:29:06 +0300

    added fucntion for dropdown blocks

--- main.js
+++ main.js
@@ -8,3 +8,28 @@ import SERVICE_3 from './img/3service.jpg'
 import SERVICE_4 from './img/4service.jpg'
 import SERVICE_5 from './img/5service.jpg'
 import SERVICE_6 from './img/6service.jpg'
+import LEAFS from './img/leafs.png'
+
+//функция для выпадающих блоков
+const accordionButton = document.querySelectorAll('.accordion');
+const allAcordionBlock = document.querySelectorAll('.accordion-block');
+accordionButton.forEach((el) => {
+    el.addEventListener('click', () => {
+        let accordionBlock = el.lastElementChild;
+        console.log(accordionBlock.closest("div[id]"))
+        if(accordionBlock.style.display === "block")
+        {
+            accordionBlock.style.display = "none";
+            accordionBlock.closest("div[id]").style.backgroundColor = "#EDF2EC"
+        } else{
+            allAcordionBlock.forEach((el) => {
+                el.style.display = "none";
+                el.closest("div[id]").style.backgroundColor = "#EDF2EC";
+            })
+            accordionBlock.style.display = "block";
+            accordionBlock.closest("div[id]").style.backgroundColor = "#D6E7D2"
+        }
+    })
+})
+
+
